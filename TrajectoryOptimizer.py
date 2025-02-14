6f413e41ff6609ca20bded3cd8bcdd3a459ccf3e
Schönherr <samuel.schoenherr@student.tugraz.at>
2025-02-14 18:45:54 +0100

    implemented quiver plots for gravity and thrust into trajectory, added experiments in 3d with masses laying in 3d or x-y plane and going from r0=-1,-1,-1 to r1=1,1,1

--- TrajectoryOptimizer.py
+++ TrajectoryOptimizer.py
@@ -4,6 +4,14 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({
+    "text.usetex": False,
+    "mathtext.fontset": "cm",
+    "font.family": "serif",
+    "axes.unicode_minus": True,
+    "font.size":11
+})
+
 class PINN(nn.Module):
 
     def __init__(self, N_INPUT, N_OUTPUT, N_NEURONS, N_LAYERS):
@@ -24,7 +32,7 @@ class PINN(nn.Module):
 
 class TrajectoryOptimizer:
 
-    def __init__(self, model, ao_xyzgm, t_colloc, t_total, r0, r1, opt_lbfgs, opt_adam, n_lbfgs=150, n_adam=0, w_physics=1):
+    def __init__(self, model, ao_xyzgm, t_colloc, t_total, r0, r1, opt_lbfgs, opt_adam, n_lbfgs=150, n_adam=0, w_physics=1, quiver_scale=10, planet_size=200):
         self.model = model  # Pinn
         self.eps = 1e-8
         self.lbfgs = opt_lbfgs
@@ -51,6 +59,8 @@ class TrajectoryOptimizer:
         self.G_plt = self.G.detach().numpy()
         self.T_plt = self.T.detach().numpy()
         self.ao_plt = ao_xyzgm
+        self.quiver_scale = quiver_scale
+        self.planet_size = planet_size
 
         # Magnitudes 
         self.a_mag = self.a.norm(dim=1).detach().numpy()
@@ -58,7 +68,7 @@ class TrajectoryOptimizer:
         self.T_mag = self.T.norm(dim=1).detach().numpy()
         
         self.plot_trajectory()
-        self.plot_forces()
+        self.plot_force_magnitudes()
         self.plot_loss()
     
     def compute_gravitational_force(self):
@@ -67,9 +77,9 @@ class TrajectoryOptimizer:
         for ao in self.ao_xyzgm:
             r_diff = self.r - ao[:3]
             denominator = (torch.linalg.norm(r_diff, dim=1) + self.eps)**1.5
-            self.G[:,0] += ao[-1] * r_diff[:,0] / denominator
-            self.G[:,1] += ao[-1] * r_diff[:,1] / denominator
-            self.G[:,2] += ao[-1] * r_diff[:,2] / denominator
+            self.G[:,0] += -ao[-1] * r_diff[:,0] / denominator
+            self.G[:,1] += -ao[-1] * r_diff[:,1] / denominator
+            self.G[:,2] += -ao[-1] * r_diff[:,2] / denominator
 
     def compute_thrust(self):
 
@@ -85,7 +95,7 @@ class TrajectoryOptimizer:
             self.a = torch.concat([ax,ay,az],dim=1)
 
             self.compute_gravitational_force()
-            self.T = self.a + self.G 
+            self.T = self.a - self.G 
         
 
     def train_model(self):
@@ -116,57 +126,73 @@ class TrajectoryOptimizer:
     def plot_loss(self):
 
         plt.figure(figsize=(8, 5))
-        plt.plot(self.loss_history)
+        plt.plot(self.loss_history, label='Loss(Training epochs)')
         plt.yscale('log')
         plt.xlim(left=0)
+        plt.xlim(right=len(self.loss_history))
         plt.xlabel("Training Epochs")
         plt.ylabel("Loss")
-        plt.title("Loss Over Training")
         plt.legend()
         plt.show()
 
     def plot_trajectory(self):
 
-        # TODO: Add forces to trajectory plot using quiver
-
         x, y, z = self.r_plt[:,0], self.r_plt[:,1], self.r_plt[:,2]
-
         self.fig_traj, self.ax_traj = plt.subplots(2,2, figsize=(10,8))
-        self.fig_traj.delaxes(self.ax_traj[1,1])
-        self.plot_masses()
-
-        self.ax_traj[0,0].scatter(-1,-1,color='r',marker='o',label=f'r(t=0)=(-1,-1,-1)')
-        self.ax_traj[0,0].scatter(1,1,color='r',marker='x',label=f'r(t=0)=(1,1,1)')
+        
+        # (x,y)
+        self.ax_traj[0,0].scatter(-1,-1,color='r',marker='o',label=r'$r(t=0)=(-1,-1,-1)$')
+        self.ax_traj[0,0].scatter(1,1,color='r',marker='x',label=r'$r(t=1)=(1,1,1)$')
         self.ax_traj[0,0].plot(x, y, label='Trajectory')
         self.ax_traj[0,0].set_xlabel('x')
         self.ax_traj[0,0].set_ylabel('y')
-        self.ax_traj[0,0].legend()
 
+        # (x,z)
         self.ax_traj[0,1].scatter(-1,-1,color='r',marker='o',label=f'r(t=0)=(-1,-1,-1)')
         self.ax_traj[0,1].scatter(1,1,color='r',marker='x',label=f'r(t=0)=(1,1,1)')
         self.ax_traj[0,1].plot(x, z)
         self.ax_traj[0,1].set_xlabel('x')
         self.ax_traj[0,1].set_ylabel('z')
 
+        # (y,z)
         self.ax_traj[1,0].scatter(-1,-1,color='r',marker='o',label=f'r(t=0)=(-1,-1,-1)')
         self.ax_traj[1,0].scatter(1,1,color='r',marker='x',label=f'r(t=0)=(1,1,1)')
         self.ax_traj[1,0].plot(y, z)
         self.ax_traj[1,0].set_xlabel('y')
         self.ax_traj[1,0].set_ylabel('z')
 
+        # Plotting gravity and thrust arrows
+        step = len(self.r_plt) // 10
+        self.r_q = self.r_plt[::step,:]
+        self.G_q = self.G_plt[::step,:]
+        self.T_q = self.T_plt[::step,:]
+        self.ax_traj[0,0].quiver(self.r_q[:,0], self.r_q[:,1], self.G_q[:,0], self.G_q[:,1], color='k', scale=self.quiver_scale, label=rf'Gravity/{self.quiver_scale}')
+        self.ax_traj[0,0].quiver(self.r_q[:,0], self.r_q[:,1], self.T_q[:,0], self.T_q[:,1], color='#FFA500',scale=self.quiver_scale, label=rf'Thrust/{self.quiver_scale}')
+        self.ax_traj[0,1].quiver(self.r_q[:,0], self.r_q[:,2], self.G_q[:,0], self.G_q[:,2], color='k', scale=self.quiver_scale, label='Gravity')
+        self.ax_traj[0,1].quiver(self.r_q[:,0], self.r_q[:,2], self.T_q[:,0], self.T_q[:,2], color='#FFA500', scale=self.quiver_scale, label='Thrust')
+        self.ax_traj[1,0].quiver(self.r_q[:,1], self.r_q[:,2], self.G_q[:,1], self.G_q[:,2], color='k', scale=self.quiver_scale, label='Gravity')
+        self.ax_traj[1,0].quiver(self.r_q[:,1], self.r_q[:,2], self.T_q[:,1], self.T_q[:,2], color='#FFA500', scale=self.quiver_scale, label='Thrust')
+
+        # Legend
+        self.plot_masses()
+        h, l = self.ax_traj[0,0].get_legend_handles_labels()
+        self.ax_traj[1,1].legend(h, l, loc='center')
+        self.ax_traj[1,1].set_frame_on(False)
+        self.ax_traj[1,1].set_xticks([])
+        self.ax_traj[1,1].set_yticks([])
+
+        plt.tight_layout()
         plt.show()
 
     def plot_masses(self):
 
+        colors = ['#006400', '#228B22', '#6B8E23']
         for i, ao in enumerate(self.ao_plt):
-            self.ax_traj[0,0].scatter(ao[0], ao[1], s=ao[3]*300, c='k', marker='o')
-            self.ax_traj[0,0].text(ao[0], ao[1] + 0.1, f'GM$_{i}$')
-            self.ax_traj[0,1].scatter(ao[0], ao[2], s=ao[3]*300, c='k', marker='o')
-            self.ax_traj[0,1].text(ao[0], ao[2] + 0.1, f'GM$_{i}$')
-            self.ax_traj[1,0].scatter(ao[1], ao[2], s=ao[3]*300, c='k', marker='o')
-            self.ax_traj[1,0].text(ao[1], ao[2] + 0.1, f'GM$_{i}$')
+            self.ax_traj[0,0].scatter(ao[0], ao[1], s=ao[3]*self.planet_size, color=colors[i], marker='o', label=f'$GM_{i+1}={ao[3]}$')
+            self.ax_traj[0,1].scatter(ao[0], ao[2], s=ao[3]*self.planet_size, color=colors[i], marker='o', label=fr'$GM_{i+1}={ao[3]}$')
+            self.ax_traj[1,0].scatter(ao[1], ao[2], s=ao[3]*self.planet_size, color=colors[i], marker='o', label=f'$GM_{i+1}={ao[3]}$')
             
-    def plot_forces(self):
+    def plot_force_magnitudes(self):
         
         plt.figure()
         plt.plot(self.t_plt, self.T_mag + self.G_mag, label='Required force magnitude')
@@ -186,10 +212,10 @@ t_total = torch.tensor(1.0, requires_grad=True)
 r0 = torch.tensor([[-1.,-1.,-1.]])
 r1 = torch.tensor([[1.,1.,1.]])
 m0 = 1.
-ao_xyzgm = [[-0.5, -1., 0., 0.2],  # astronomic objects: x, y, z gravitational mass
-            [-0.2, 0.4, 0., 0.4],
-            [ 0.8, 0.3, 0., 0.3]]
-
+ao_xyzgm = [[-0.5, -1., -0.1, 0.5],  # astronomic objects: x, y, z gravitational mass
+            [-0.2, 0.4, 0.2, 1.0],
+            [ 0.8, 0.3, 1.4, 0.5]]
+   
 # Initialize model
 pinn = PINN(1, 3, 50, 3)
 params = list(pinn.parameters())
@@ -202,6 +228,6 @@ seed = 123
 torch.manual_seed(seed)
 np.random.seed(seed)
 
-obj = TrajectoryOptimizer(pinn, ao_xyzgm, t_colloc, t_total, r0, r1, optimizer_lbfgs, optimizer_adam, w_physics=7, n_adam=200)
+obj = TrajectoryOptimizer(pinn, ao_xyzgm, t_colloc, t_total, r0, r1, optimizer_lbfgs, optimizer_adam, w_physics=1, n_adam=100)
 
 # %%
