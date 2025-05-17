88dd721d9b84623d763e3048fb6e9416c5744a8c
schnhrrr <samuel.schoenherr@hotmail.com>
2025-05-17 18:17:17 +0200

    fixed typo result: --> result

--- src/runner.py
+++ src/runner.py
@@ -6,6 +6,6 @@ def run_experiment(config):
     model = PINN(**config['pinn'])  # Build the PINN
     opt = TrajectoryOptimizer(model, **config['optimizer'])  # Train the model
     res = TrajectoryResult(config['label'], opt)  # Extract the results
-    return {"label:": config['label'], 
+    return {"label": config['label'], 
             "result": res,
             **config.get("plotting",{})}  # Return the results
