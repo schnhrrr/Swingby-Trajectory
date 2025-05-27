c0bc8e925a7afab2ca862a239a03bcd62f1a802f
schnhrrr <samuel.schoenherr@hotmail.com>
2025-05-27 14:42:20 +0200

    better HP for vanilla position 3d

--- experiments/vanilla_vs_position_3d.py
+++ experiments/vanilla_vs_position_3d.py
@@ -6,6 +6,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config.config_3d import position3d_config, vanilla3d_config
 from src.runner import run_experiment
 
+position3d_config['optimizer']['n_adam'] = 1000
+position3d_config['optimizer']['n_lbfgs'] = 500
+vanilla3d_config['optimizer']['n_adam'] = 2_000
+vanilla3d_config['optimizer']['n_lbfgs'] = 1_000
+
 results = []
 for config in [position3d_config, vanilla3d_config]:
     result = run_experiment(config)
@@ -13,5 +18,5 @@ for config in [position3d_config, vanilla3d_config]:
     results.append(result)
 
 from src.plotter import TrajectoryPlotter
-plotter = TrajectoryPlotter(results, dim=3, figsize=(6, 6))
+plotter = TrajectoryPlotter(results, dim=3, figsize=(7, 7))
 plotter.plot_all()
