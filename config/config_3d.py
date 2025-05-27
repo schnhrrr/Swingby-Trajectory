6451ae770bf7d7ed119d2fa38fe8221fba0df9df
schnhrrr <samuel.schoenherr@hotmail.com>
2025-05-27 13:57:35 +0200

    vanilla vs position 3d

--- config/config_3d.py
+++ config/config_3d.py
@@ -5,7 +5,7 @@ from config.shared_parameters import x0_3d, xN_3d, ao_3d, t_colloc, t_total
 
 position3d_config = {
     "label": "Position-transformed",
-    "seed": 123,
+    "seed": 2809,
     "pinn": {
         "N_INPUT": 1,
         "N_OUTPUT": 3,
@@ -22,8 +22,8 @@ position3d_config = {
         "rN": xN_3d,
         "opt_adam": partial(torch.optim.Adam, lr=1e-3),
         "opt_lbfgs": partial(torch.optim.LBFGS, max_iter=10, lr=0.1),
-        "n_adam": 1_000,
-        "n_lbfgs": 0,
+        "n_adam": 0,
+        "n_lbfgs": 200,
         "w_physics": 1.,
         "w_bc": 0,
     },
@@ -36,7 +36,7 @@ position3d_config = {
 
 vanilla3d_config = {
     "label": "Vanilla",
-    "seed": 123,
+    "seed": 2809,
     "pinn": {
         "N_INPUT": 1,
         "N_OUTPUT": 3,
@@ -53,8 +53,8 @@ vanilla3d_config = {
         "rN": xN_3d,
         "opt_adam": partial(torch.optim.Adam, lr=1e-3),
         "opt_lbfgs": partial(torch.optim.LBFGS, max_iter=10, lr=0.1),
-        "n_adam": 1_000,
-        "n_lbfgs": 0,
+        "n_adam": 2_000,
+        "n_lbfgs": 700,
         "w_physics": 1.,
         "w_bc": 3.5,
     },
---
+++ experiments/vanilla_vs_position_3d/vanilla_vs_position_3d.py
@@ -0,0 +1,17 @@
+#%%
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from config.config_3d import position3d_config, vanilla3d_config
+from src.runner import run_experiment
+
+results = []
+for config in [position3d_config, vanilla3d_config]:
+    result = run_experiment(config)
+    print(f"Training of {config['label']} completed.")
+    results.append(result)
+
+from src.plotter import TrajectoryPlotter
+plotter = TrajectoryPlotter(results, dim=3, figsize=(7, 7))
+plotter.plot_all()
