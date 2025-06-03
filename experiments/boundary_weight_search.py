225aa7e42c58e12ae5d2bcdb0c8852043c7a795c
schnhrrr <samuel.schoenherr@hotmail.com>
2025-06-03 10:34:50 +0200

    boundary weight search evaluation

--- experiments/boundary_weight_search.py
+++ experiments/boundary_weight_search.py
@@ -14,10 +14,10 @@ w_bc_list = []
 min_bc_loss = []
 min_total_loss = []
 
-weights = np.exp(np.linspace(np.log(1e-3), np.log(1e3), 1_000))
+weights = np.exp(np.linspace(np.log(1e-3), np.log(1e3), 1_000))[292:]
 
 for i, w_bc in enumerate(weights):
-    print(i)
+    print(i+292)
     vanilla2d_config['optimizer']['w_bc'] = w_bc
     result = run_experiment(vanilla2d_config)
     idx.append(i)
@@ -28,3 +28,23 @@ for i, w_bc in enumerate(weights):
 data = np.column_stack([idx, w_bc_list, min_bc_loss, min_total_loss])
 np.savetxt("weights.csv", data, delimiter=",", fmt="%.4f", header="Idx,w_bc,min-bc-loss,min-total-loss", comments='')
 
+# %%
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt('weights2d.csv', delimiter=',', skip_header=True)
+
+w_bc = data[:,1]
+loss_bc = data[:,2]
+loss_total = data[:,3]
+
+min_loss = min(loss_total[400:])
+min_loss_idx = np.where(loss_total == min_loss)
+
+plt.figure()
+plt.plot(w_bc, loss_total, label='Smallest Total Loss')
+#plt.plot(w_bc, loss_bc, label='Smallest BC Loss')
+plt.scatter(w_bc[min_loss_idx[0][-1]], min_loss, marker='x', color='r', label=r'$\omega_{BC}='+f'{w_bc[min_loss_idx[0][-1]]}$', s=50)
+plt.xscale('log')
+plt.yscale('log')
+plt.legend()
+plt.legend()
