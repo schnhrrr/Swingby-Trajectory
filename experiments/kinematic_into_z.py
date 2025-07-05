9d86f35c773c157f21c7576fc26b3e42555d0392
schnhrrr <samuel.schoenherr@hotmail.com>
2025-07-05 17:17:07 +0200

    save stable situation into z

--- experiments/kinematic_into_z.py
+++ experiments/kinematic_into_z.py
@@ -4,22 +4,27 @@ import sys
 import torch
 from functools import partial
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from config.config_3d import kinematic3d_config
 from config.shared_parameters import x0_3d, xN_3d, v0_3d, vN_3d
 from config.transform_functions import kinematic_fn
 from src.runner import run_experiment
 
-kinematic3d_config['optimizer']['n_adam'] = 1000 #3000
+kinematic3d_config['optimizer']['n_adam'] = 50 #3000
 kinematic3d_config['optimizer']['opt_adam'] = partial(torch.optim.Adam, lr=1e-3)
-kinematic3d_config['optimizer']['n_lbfgs'] = 200
+kinematic3d_config['optimizer']['n_lbfgs'] = 1
 
 #v0_3d = torch.tensor([[ 1., 1., 1.]])
 #vN_3d = torch.tensor([[1., 1., -1.]])
-#v0_3d = torch.tensor([[ 0.4308,  1.0814,  1.1313]])
-#vN_3d = torch.tensor([[ 0.7383,  0.5656, -1.1591]])
-v0_3d = torch.tensor([[ 0.8,  1.4,  1.6]])
-vN_3d = torch.tensor([[ 1.2,  0.6, -1.]])
+v0_3d = torch.tensor([[ 0.4308,  1.0814,  1.1313]])
+vN_3d = torch.tensor([[ 0.7383,  0.5656, -1.1591]])
+#v0_3d = torch.tensor([[ 0.8,  1.4,  -1.]])
+#vN_3d = torch.tensor([[ 1.2,  0.6, 1.]])
 
+import math
+sq2 = math.sqrt(2)*2
+v0_3d = torch.tensor([[sq2, sq2,0.]])
+vN_3d = torch.tensor([[sq2, sq2,0.]])
 
 kinematic3d_config['pinn']['output_transform_fn'] = partial(
     kinematic_fn,
@@ -37,5 +42,3 @@ for config in [kinematic3d_config]:
 from src.plotter import TrajectoryPlotter
 plotter = TrajectoryPlotter(results, dim=3, figsize=(7, 7))
 plotter.plot_all()
-
-# %%
