b605432ed894378f902bcac6c3085d8ae2001e6c
schnhrrr <samuel.schoenherr@hotmail.com>
2025-07-05 13:27:34 +0200

    transfer learning / and qualitative v guess

--- experiments/kinematic_into_z.py
+++ experiments/kinematic_into_z.py
@@ -4,18 +4,27 @@ import sys
 import torch
 from functools import partial
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from config.config_3d import position3d_config, vanilla3d_config, kinematic3d_config
+from config.config_3d import kinematic3d_config
+from config.shared_parameters import x0_3d, xN_3d, v0_3d, vN_3d
+from config.transform_functions import kinematic_fn
 from src.runner import run_experiment
 
-kinematic3d_config['optimizer']['n_adam'] = 2000
-kinematic3d_config['optimizer']['n_lbfgs'] = 150
-v0_3d = torch.tensor([[ 0.2479,  0.9203,  1.2174]])
-vN_3d = torch.tensor([[ 0.5784,  0.3899, -1.2156]])
+kinematic3d_config['optimizer']['n_adam'] = 1000 #3000
+kinematic3d_config['optimizer']['opt_adam'] = partial(torch.optim.Adam, lr=1e-3)
+kinematic3d_config['optimizer']['n_lbfgs'] = 200
 
-kinematic3d_config['optimizer']['output_transform_fn'] = partial(
-    kinematic3d_config['pinn']['output_transform_fn'],
-    x0=kinematic3d_config['optimizer']['r0'],
-    xN=kinematic3d_config['optimizer']['rN'],
+#v0_3d = torch.tensor([[ 1., 1., 1.]])
+#vN_3d = torch.tensor([[1., 1., -1.]])
+#v0_3d = torch.tensor([[ 0.4308,  1.0814,  1.1313]])
+#vN_3d = torch.tensor([[ 0.7383,  0.5656, -1.1591]])
+v0_3d = torch.tensor([[ 0.8,  1.4,  1.6]])
+vN_3d = torch.tensor([[ 1.2,  0.6, -1.]])
+
+
+kinematic3d_config['pinn']['output_transform_fn'] = partial(
+    kinematic_fn,
+    x0=x0_3d,
+    xN=xN_3d,
     v0=v0_3d,
     vN=vN_3d)
 
