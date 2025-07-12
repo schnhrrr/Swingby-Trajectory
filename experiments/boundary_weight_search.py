c3ad5950132b7919d42523907d12768b4ab5e4eb
schnhrrr <samuel.schoenherr@hotmail.com>
2025-07-12 12:41:07 +0200

    start 2d grid search for dynamic TOF

--- experiments/boundary_weight_search.py
+++ experiments/boundary_weight_search.py
@@ -7,37 +7,37 @@ from config.config_2d import vanilla2d_config
 from src.runner import run_experiment
 
 vanilla2d_config['optimizer']['n_adam'] = 2000
-vanilla2d_config['optimizer']['n_lbfgs'] = 500
+vanilla2d_config['optimizer']['n_lbfgs'] = 1000
 
 idx = []
 w_bc_list = []
 min_bc_loss = []
 min_total_loss = []
 
-weights = np.exp(np.linspace(np.log(1e-3), np.log(1e3), 1_000))[292:]
+weights = np.exp(np.linspace(np.log(1e-3), np.log(1e1), 666))
 
 for i, w_bc in enumerate(weights):
-    print(i+292)
+    print(i+1)
+    idx.append(i+1)
     vanilla2d_config['optimizer']['w_bc'] = w_bc
     result = run_experiment(vanilla2d_config)
-    idx.append(i)
     min_bc_loss.append(min(result['result'].loss_bc))
     min_total_loss.append(min(result['result'].loss))
     w_bc_list.append(w_bc)
 
 data = np.column_stack([idx, w_bc_list, min_bc_loss, min_total_loss])
-np.savetxt("weights.csv", data, delimiter=",", fmt="%.4f", header="Idx,w_bc,min-bc-loss,min-total-loss", comments='')
+np.savetxt("weights2d_dynamic.csv", data, delimiter=",", fmt="%.4f", header="Idx,w_bc,min-bc-loss,min-total-loss", comments='')
 
 # %%
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt('weights2d.csv', delimiter=',', skip_header=True)
+data = np.genfromtxt('weights2d_dynamic.csv', delimiter=',', skip_header=True)
 
 w_bc = data[:,1]
 loss_bc = data[:,2]
 loss_total = data[:,3]
 
-min_loss = min(loss_total[400:])
+min_loss = min(loss_total)
 min_loss_idx = np.where(loss_total == min_loss)
 
 plt.figure()
