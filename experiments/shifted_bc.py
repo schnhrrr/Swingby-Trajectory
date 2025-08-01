978823ced9463de9fbb35528e39e1d9fe3f9b01d
schnhrrr <samuel.schoenherr@hotmail.com>
2025-08-01 13:43:35 +0200

    redo shifted bc experiment with trainable TOF

--- experiments/shifted_bc.py
+++ experiments/shifted_bc.py
@@ -5,22 +5,22 @@ from functools import partial
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from config.config_3d import position3d_config
-from src.runner import run_experiment
-from config.transform_functions import position_3d
+from src.runner import run_experiment, export_results
+from config.transform_functions import position_fn
 import torch
-
+import numpy as np
 
 x0_A = lambda A: torch.tensor([[-1., -1., A]])
 xN_A = lambda A: torch.tensor([[1., 1., -A]])
 
-position3d_config['optimizer']['n_adam'] = 1000
-position3d_config['optimizer']['n_lbfgs'] = 500
+position3d_config['optimizer']['n_adam'] = 1
+position3d_config['optimizer']['n_lbfgs'] = 5
 del position3d_config['plotting']['color']
 
 results = []
-for A in [0, 0.1, 0.2, 0.3, 0.4, 0.5]:
+for A in np.linspace(0.0, 1.0, 11):
     position3d_config['label'] = f"A={A}"
-    position3d_config['pinn']['output_transform_fn'] = partial(position_3d, x0=x0_A(A), xN=xN_A(A))
+    position3d_config['pinn']['output_transform_fn'] = partial(position_fn, x0=x0_A(A), xN=xN_A(A))
     position3d_config['optimizer']['r0'] = x0_A(A)
     position3d_config['optimizer']['rN'] = xN_A(A)
 
@@ -28,6 +28,8 @@ for A in [0, 0.1, 0.2, 0.3, 0.4, 0.5]:
     print(f"Training of {position3d_config['label']} completed.")
     results.append(result)
 
+export_results(results, "shifted_bc_3d_results.pkl")
+
 from src.plotter import TrajectoryPlotter
-plotter = TrajectoryPlotter(results, dim=3, figsize=(7, 7))
-plotter.plot_all()
+plotter = TrajectoryPlotter(results, dim=3, figsize=(7, 7), fig_prefix="shifted_bc_3d")
+plotter.plot_all(plot_quiver=False)
