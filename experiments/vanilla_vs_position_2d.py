fae861fff6023ea13dfc9625a634d040a1b4e424
schnhrrr <samuel.schoenherr@hotmail.com>
2025-09-09 18:17:36 +0200

    rerun after kinematic bug: dynamic grid search, vanilla vs position

--- experiments/vanilla_vs_position_2d.py
+++ experiments/vanilla_vs_position_2d.py
@@ -6,8 +6,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from config.config_2d import position2d_config, vanilla2d_config
 from src.runner import run_experiment
 
-vanilla2d_config['optimizer']['w_bc'] = 1.77
-
 if static_experiment := False:  # Set True for static time experiment
     del position2d_config['extra_parameters']
     del vanilla2d_config['extra_parameters']
@@ -15,11 +13,16 @@ if static_experiment := False:  # Set True for static time experiment
     position2d_config['optimizer']['n_adam'] = 200
     position2d_config['optimizer']['n_lbfgs'] = 200
 
+vanilla2d_config['optimizer']['w_bc'] = 1.67
+vanilla2d_config['optimizer']['n_lbfgs'] = 10000
+position2d_config['optimizer']['n_lbfgs'] = 10000
+
 results = []
 for config in [position2d_config, vanilla2d_config]:
     result = run_experiment(config)
     print(f"Training of {config['label']} completed.")
     results.append(result)
+    print(f'E_kin,0 = {sum(result['result'].v[0]**2)/2}')
 
 from src.plotter import TrajectoryPlotter
 plotter = TrajectoryPlotter(results, dim=2, figsize=(7, 7), fig_prefix="position_vanilla_2d")
--- experiments/vanilla_vs_position_3d.py
+++ experiments/vanilla_vs_position_3d.py
@@ -15,18 +15,20 @@ if static_experiment := False:
     vanilla3d_config['optimizer']['n_lbfgs'] = 500
     vanilla3d_config['optimizer']['w_bc'] = 68.36
 
-vanilla3d_config['optimizer']['w_bc'] = 3.58
-position3d_config['optimizer']['n_adam'] = 1000
-position3d_config['optimizer']['n_lbfgs'] = 700
+vanilla3d_config['optimizer']['w_bc'] = 2.997
 vanilla3d_config['optimizer']['n_adam'] = 2_000
-vanilla3d_config['optimizer']['n_lbfgs'] = 1_500
+vanilla3d_config['optimizer']['n_lbfgs'] = 10000
+
+position3d_config['optimizer']['n_adam'] = 1000
+position3d_config['optimizer']['n_lbfgs'] = 10000
 
 results = []
 for config in [position3d_config, vanilla3d_config]:
     result = run_experiment(config)
     print(f"Training of {config['label']} completed.")
     results.append(result)
+    print(f'E_kin,0 = {sum(result['result'].v[0]**2)/2}')
 
 from src.plotter import TrajectoryPlotter
-plotter = TrajectoryPlotter(results, dim=3, figsize=(7, 7))
+plotter = TrajectoryPlotter(results, dim=3, figsize=(7, 7), fig_prefix="position_vanilla_3d")
 plotter.plot_all()
