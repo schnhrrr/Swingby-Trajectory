90032fda2684518addf953909f96a41af61ff3ab
schnhrrr <samuel.schoenherr@hotmail.com>
2025-09-22 21:06:29 +0200

    redo shifted bc exp after kinematic bug

--- experiments/shifted_bc.py
+++ experiments/shifted_bc.py
@@ -13,13 +13,13 @@ import numpy as np
 x0_A = lambda A: torch.tensor([[-1., -1., A]])
 xN_A = lambda A: torch.tensor([[1., 1., -A]])
 
-position3d_config['optimizer']['n_adam'] = 1
-position3d_config['optimizer']['n_lbfgs'] = 5
+position3d_config['optimizer']['n_adam'] = 1_000
+position3d_config['optimizer']['n_lbfgs'] = 10_000
 del position3d_config['plotting']['color']
 
 results = []
 for A in np.linspace(0.0, 1.0, 11):
-    position3d_config['label'] = f"A={A}"
+    position3d_config['label'] = f"A={A:.1f}"
     position3d_config['pinn']['output_transform_fn'] = partial(position_fn, x0=x0_A(A), xN=xN_A(A))
     position3d_config['optimizer']['r0'] = x0_A(A)
     position3d_config['optimizer']['rN'] = xN_A(A)
